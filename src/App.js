e961e2d3d2f47159d43c981139612746c222fc47
lalinda74 <diaslalinda@gmail.com>
2021-10-03 13:28:05 +0530

    TB-0000 commit project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import styled, { ThemeProvider } from "styled-components";
+import WebFont from 'webfontloader';
+import ThemeCard from './components/ThemeCard';
+import { GlobalStyles } from './theme/globalStyles';
+import { useTheme } from './theme/useTheme';
+
+// create a container
+const Container = styled.div`
+  margin: 5px auto 5px auto;
+`;
 
 function App() {
+  // get the selected theme and font-list
+  const { theme, themeLoaded, getFonts } = useTheme();
+  const [ selectedTheme, setSelectedTheme ] = useState(theme);
+  const [newTheme, setNewTheme] = useState();
+
+  useEffect(() => {
+    setSelectedTheme(theme);
+  }, [themeLoaded]);
+
+  // load all the fonts
+  useEffect(() => {
+    WebFont.load({
+      google: {
+        families: getFonts()
+      }
+    })
+  });
+
+  const createTheme = newTheme => {
+    console.log(newTheme);
+    setNewTheme(newTheme);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {
+      themeLoaded && <ThemeProvider theme={ selectedTheme }>
+        <GlobalStyles />
+        <Container style={{fontFamily: selectedTheme.font}}>
+          <h1>Theme Builder</h1>
+          <p>
+            This is a theming system with a Theme Switcher and Theme Builder.
+            Do you want to see the source code? <a href="/" target="_blank">GitHub</a>
+          </p>
+          <ThemeCard setter={ setSelectedTheme } newTheme={ newTheme } />
+        </Container>
+      </ThemeProvider>
+    }
+    </>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,9 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+// loading themes
+import * as themes from './theme/schema.json';
+
+// loading storage utils
+import { setToLS } from './utils/storage';
+
+const Index = () => {
+  setToLS('all-themes', themes.default);
+  return (
+    <App />
+  )
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>,
   document.getElementById('root')
 );
